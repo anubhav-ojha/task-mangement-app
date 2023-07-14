4fc16229b0882aafe9cc5abebdfc49a12e63c50d
Anubhav Ojha <anubhavojha9@gmail.com>
2023-07-14 18:22:52 +0530

    setup in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TaskListComponent,
-    AddTaskComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
